7f10ce3ad6e3a1563bfee0d574889133325a5d16
sijun-yang <yangsijun5528@gmail.com>
2026-08-08 19:16:50 +0900

    Use slices in iterators1

--- exercises/18_iterators/iterators1.rs
+++ exercises/18_iterators/iterators1.rs
@@ -10,9 +10,9 @@ fn main() {
 mod tests {
     #[test]
     fn iterators() {
-        let my_fav_fruits = ["banana", "custard apple", "avocado", "peach", "raspberry"];
+        let my_fav_fruits = &["banana", "custard apple", "avocado", "peach", "raspberry"];
 
-        // TODO: 배열에 대한 반복자를 만들어봐.
+        // TODO: Create an iterator over the slice.
         let mut fav_fruits_iterator = todo!();
 
         assert_eq!(fav_fruits_iterator.next(), Some(&"banana"));
